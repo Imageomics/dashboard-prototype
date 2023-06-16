4174d7535aca368978bfc9596d0b53230f36fe78
egrace479 <e.campolongo479@gmail.com>
2023-06-16 14:52:06 -0400

    Prototype Map of sample distribution by locality (lat/lon)

---
+++ test_components/graphs.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import plotly.express as px
+
+
+def make_map(df):
+    '''
+    Generates interactive graph of Species and Subspecies by location.
+    
+    Parameters:
+    -----------
+    df - dataframe of specimens
+
+    Returns: 
+    --------
+    fig - Map of their locations
+    '''
+    fig = px.scatter_geo(df,
+                        lat = df.lat,
+                        lon = df.lon,
+                        projection = "natural earth",
+                        hover_data = ["Species", "Subspecies"],
+                        size = df.samples_at_locality,
+                        color = "Subspecies",
+                        color_discrete_sequence = px.colors.qualitative.Bold)
+    fig.update_geos(fitbounds = "locations",
+                    showrivers = True,
+                    showlakes = True,
+                    showland = True, landcolor = "wheat",
+                    showocean = True, oceancolor = "LightBlue")
+    return fig
